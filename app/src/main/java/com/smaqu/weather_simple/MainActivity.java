75bcea63b6a38ae8745f7ec30b46cbf82325052a
SmaQu <maciej.smolik95@gmail.com>
2018-02-26 19:53:49 +0100

    Create main view, downloadtask

--- app/src/main/java/com/smaqu/weather_simple/MainActivity.java
+++ app/src/main/java/com/smaqu/weather_simple/MainActivity.java
@@ -1,13 +1,101 @@
 package com.smaqu.weather_simple;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText city;
+    private Button searchBtn;
+    private TextView weather;
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                String weatherInfo = jsonObject.getString("weather");
+
+                JSONArray array = new JSONArray(weatherInfo);
+                for (int i = 0; i < array.length(); i++) {
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        city = findViewById(R.id.et_city);
+        searchBtn = findViewById(R.id.bt_search);
+        weather = findViewById(R.id.tv_weather);
+
+        searchBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_search:
+                findWeather();
+                break;
+        }
+    }
+
+    private void findWeather() {
+        city.getText();
+
+        DownloadTask task = new DownloadTask();
     }
 }
