5802b7b536b22a6ccae9f506e4b5d117fb0f7da8
SmaQu <maciej.smolik95@gmail.com>
2018-03-02 20:06:30 +0100

    And mini icons, progressbar, error checker

--- app/src/main/java/com/smaqu/weather_simple/MainActivity.java
+++ app/src/main/java/com/smaqu/weather_simple/MainActivity.java
@@ -1,14 +1,20 @@
 package com.smaqu.weather_simple;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,11 +28,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextView.OnEditorActionListener {
 
     private EditText city;
     private Button searchBtn;
     private TextView weather;
+    private ProgressBar progressBar;
+    private static final String TAG = "MainActivity";
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
@@ -64,28 +72,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             try {
 
                 String message = "";
+                if (result != null) {
+                    JSONObject jsonObject = new JSONObject(result);
+                    JSONArray array = jsonObject.getJSONArray("list");
+                    for (int i = 0; i < 1; i++) {
 
-                JSONObject jsonObject = new JSONObject(result);
-                String weatherInfo = jsonObject.getString("weather");
+                        JSONObject jsonPart = array.getJSONObject(i);
+                        String main = "";
+                        String description = "";
 
-                JSONArray array = new JSONArray(weatherInfo);
-                for (int i = 0; i < array.length(); i++) {
+                        JSONObject mainPart = jsonPart.getJSONObject("main");
+                        JSONArray weatherPart = jsonPart.getJSONArray("weather");
 
-                    JSONObject jsonPart = array.getJSONObject(i);
-                    String main = "";
-                    String description = "";
+                        for (int y = 0; y < weatherPart.length(); y++) {
 
-                    main = jsonPart.getString("main");
-                    description = jsonPart.getString("description");
+                            JSONObject weatherType = weatherPart.getJSONObject(y);
+                            description = weatherType.getString("main");
+                        }
 
-                    if (!Objects.equals(main, "") && !Objects.equals(description, "")) {
-                        message += main + ": " + description + "\r\n";
+                        main = mainPart.getString("temp");
+
+                        if (!Objects.equals(main, "") && !Objects.equals(description, "")) {
+                            message += main + "C " + description + "\r\n";
+                        }
                     }
-                }
-
-                if(!Objects.equals(message, "")) {
-                    weather.setVisibility(View.VISIBLE);
-                    weather.setText(message);
+                    if (!Objects.equals(message, "")) {
+                        progressBar.setVisibility(View.GONE);
+                        weather.setVisibility(View.VISIBLE);
+                        weather.setText(message);
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "No city found", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -101,8 +119,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         city = findViewById(R.id.et_city);
         searchBtn = findViewById(R.id.bt_search);
         weather = findViewById(R.id.tv_weather);
-
+        progressBar = findViewById(R.id.progressBar);
         searchBtn.setOnClickListener(this);
+        city.setOnEditorActionListener(this);
     }
 
     @Override
@@ -114,7 +133,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+            findWeather();
+            return true;
+        }
+        return false;
+    }
+
     private void findWeather() {
+
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(city.getWindowToken(), 0);
+        weather.setVisibility(View.GONE);
 
         DownloadTask task = new DownloadTask();
         Uri.Builder builder = new Uri.Builder();
@@ -124,8 +156,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .appendPath("2.5")
                 .appendPath("forecast")
                 .appendQueryParameter("q", city.getText().toString())
+                .appendQueryParameter("units", "metric")
                 .appendQueryParameter("APPID", getResources().getString(R.string.appId));
         String url = builder.build().toString();
+        Log.e(TAG, "URL: " + url);
+        progressBar.setVisibility(View.VISIBLE);
         task.execute(url);
     }
 }
